532bf8f7263cdba1374b1ed753835177938547bd
bravemaster619 <bravemaster619@hotmail.com>
2020-03-27 20:33:03 +0800

    dependency check before enable

--- PluginManager.php
+++ PluginManager.php
@@ -2,6 +2,9 @@
 namespace Plugin\PointsOnReferral;
 
 use Eccube\Plugin\AbstractPluginManager;
+use Plugin\PointsOnReferral\Exception\DependencyNotFoundException;
+use Plugin\PointsOnReferral\ServiceProvider\PointsOnReferralServiceProvider;
+use Plugin\PointsOnSignup\Service\UtilService;
 
 class PluginManager extends AbstractPluginManager {
 
@@ -14,6 +17,18 @@ class PluginManager extends AbstractPluginManager {
     }
 
     public function enable($config, $app) {
+        // check if Point plugin is installed
+        $UtilService = new UtilService($app);
+        if (!$UtilService->checkInstallPlugin('Point')) {
+            $app->error(function(\Exception $e, $code) use ($app) {
+                if ($e instanceof DependencyNotFoundException) {
+                    PointsOnReferralServiceProvider::registerMessage($app);
+                    $app->addError($app['translator']->trans('admin.plugin.enable.error.point_not_found'), 'admin');
+                    return $app->redirect($app->url('admin_store_plugin'));
+                }
+            });
+            throw new DependencyNotFoundException();
+        }
         $this->migrationSchema($app, __DIR__ . '/Resource/doctrine/migration', $config['code']);
     }
 
