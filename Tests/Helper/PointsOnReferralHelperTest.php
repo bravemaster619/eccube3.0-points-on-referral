d5f3de38cae0c10befe22d9bbde1f2ec49769dce
bravemaster619 <bravemaster619@hotmail.com>
2020-03-28 05:24:24 +0800

    test for helper

---
+++ Tests/Helper/PointsOnReferralHelperTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Eccube\Tests\Helper;
+
+use Eccube\Tests\EccubeTestCase;
+use Plugin\PointsOnReferral\Helper\PointsOnReferralHelper;
+
+class PointsOnReferralHelperTest extends EccubeTestCase {
+
+    public function testGenerateReferralCode() {
+        $PoRHelper = new PointsOnReferralHelper($this->app);
+        $referral_code = $PoRHelper->generateReferralCode();
+        $this->assertNotEmpty($referral_code);
+    }
+
+}
