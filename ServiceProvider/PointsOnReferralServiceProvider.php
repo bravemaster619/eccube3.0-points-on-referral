59874833d1e08242f4012cb515c3d1891cd055db
bravemaster619 <bravemaster619@hotmail.com>
2020-03-27 20:32:29 +0800

    register messages

--- ServiceProvider/PointsOnReferralServiceProvider.php
+++ ServiceProvider/PointsOnReferralServiceProvider.php
@@ -2,18 +2,35 @@
 
 namespace Plugin\PointsOnReferral\ServiceProvider;
 
-use Eccube\Application;
 use Silex\Application as BaseApplication;
 use Silex\ServiceProviderInterface;
 
 class PointsOnReferralServiceProvider implements ServiceProviderInterface {
 
     public function register(BaseApplication $app) {
-        // TODO: Implement register() method.
+        self::registerMessage($app);
+
     }
 
     public function boot(BaseApplication $app) {
-        // TODO: Implement boot() method.
+
+    }
+
+    public static function registerMessage(BaseApplication $app) {
+        $app['translator'] = $app->share(
+            $app->extend(
+                'translator',
+                function ($translator, \Silex\Application $app) {
+                    $translator->addLoader('yaml', new \Symfony\Component\Translation\Loader\YamlFileLoader());
+                    $file = __DIR__.'/../Resource/locale/message.'.$app['locale'].'.yml';
+                    if (file_exists($file)) {
+                        $translator->addResource('yaml', $file, $app['locale']);
+                    }
+
+                    return $translator;
+                }
+            )
+        );
     }
 
 }
